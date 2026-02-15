b04c60128d94414becef942940b5ad0e4dce1a5f
Andrew Luu <aweandrew5531@gmail.com>
2026-02-14 16:43:10 -0800

    Add MaxHeapBuilder class

--- MaxHeapBuilder.java
+++ MaxHeapBuilder.java
@@ -11,6 +11,22 @@ class WordFreq {
     }
 }
 
+public class MaxHeapBuilder {
+    public static void main(String[] args) {
+        WordFreq[] data = {
+            new WordFreq("happy",400),
+            new WordFreq("satisfied",100),
+            new WordFreq("neutral",300),
+            new WordFreq("would buy again",200),
+            new WordFreq("terrible",160),
+            new WordFreq("inconvenient",900),
+            new WordFreq("difficult to use",100),
+            new WordFreq("easy to use",140),
+            new WordFreq("would recommend to friends",800),
+            new WordFreq("visit the store",700)
+        };
+    }
+    
 
 
 
